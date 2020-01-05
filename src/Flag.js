68becd9cdf75dd9aabd7ff3718f619a6625a69de
sunlee <sunlee128@gmail.com>
2020-01-05 06:38:46 +0200

    Flag, QustionBar component added

---
+++ src/Flag.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Flag extends Component {
+	render() {
+		return (
+			<div>
+				<h1>flag</h1>
+			</div>
+		);
+	}
+}
+
+export default Flag;
---
+++ src/QuestionBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class QuestionBar extends Component {
+  render () {
+    return (
+      <div>
+        <h1>QuestionBar</h1>
+      </div>
+    )
+  }
+}
+export default QuestionBar
