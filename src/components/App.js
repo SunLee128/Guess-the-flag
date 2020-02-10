7149276a34cf51a2c610a1b57fe1dabbd7a2bf3c
sunlee <sunlee128@gmail.com>
2020-02-11 09:34:37 +1100

    components game logic added

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import QuestionBar from 'components/QuestionBar'
-import Flag from 'components/Flag';
-import Game from 'components/Game';
+import React, { Component } from 'react';
+import Game from './Game';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <QuestionBar />
-      <Flag />
-      <Game />
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="flag-app">
+        <header className="title-header">
+          <h1 className="title-text">Guess The Flag</h1>
+        </header>
+        <Game />
+      </div>
+    );
+  }
 }
 
 export default App;
